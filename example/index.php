6865b79abeab770a4b07b8246f5f038d8ae83adf
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-09-01 17:55:19 +0700

    Implement

--- example/index.php
+++ example/index.php
@@ -3,27 +3,30 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $hideMyAssApi = new \HideMyAssAPI\HideMyAssAPI();
 /** @var \HideMyAssAPI\Proxy[] $proxyList */
 $proxyList = $hideMyAssApi->getProxy();
-$firstProxy = $proxyList[0];
 
-$multi_curl = new \Curl\MultiCurl();
-//$multi_curl->setOpt(CURLOPT_NOBODY,true);
-//$multi_curl->setOpt(CURLOPT_FILETIME,true);
-$multi_curl->success(function($instance) {
-    echo '<li>call to "' . $instance->url . '" was successful.' . "</li>";
-    echo '<li>response: ' . $instance->response . "</li>";
-});
-$multi_curl->error(function($instance) {
-    echo '<li>call to "' . $instance->url . '" was unsuccessful.' . "</li>";
-    echo '<li>error code: ' . $instance->errorCode . "</li>";
-    echo '<li>error message: ' . $instance->errorMessage . "</li>";
-});
+for($index = 0;$index<50; $index++){
+    makeRequest($proxyList[$index]);
+}
 
-$multi_curl->complete(function($instance) {
-    echo '<li>call completed</li>';
-});
+function makeRequest($proxy){
+    $multi_curl = new \Curl\MultiCurl();
+    $multi_curl->success(function($instance) {
+        echo '<li>call to "' . $instance->url . '" was successful.' . "</li>";
+        /*echo '<li>response: ' . $instance->response . "</li>";*/
+    });
+    $multi_curl->error(function($instance) {
+        echo '<li>call to "' . $instance->url . '" was unsuccessful.' . "</li>";
+        echo '<li>error code: ' . $instance->errorCode . "</li>";
+        echo '<li>error message: ' . $instance->errorMessage . "</li>";
+    });
 
-$multi_curl->addGet('https://downloads.wordpress.org/plugin/wp-basketjs.zip');
+    $multi_curl->complete(function($instance) {
+        echo '<li>call completed</li>';
+    });
 
-$multi_curl->setOpt(CURLOPT_PROXY, $firstProxy->toString());
-$multi_curl->setOpt(CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
-$multi_curl->start();
+    $multi_curl->addGet('http://laptrinh.senviet.org');
+
+    $multi_curl->setOpt(CURLOPT_PROXY, $proxy->toString());
+    $multi_curl->setOpt(CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+    $multi_curl->start();
+}
